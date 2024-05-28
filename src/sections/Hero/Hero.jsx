45c9249b7f7b9835d2c72ce19410e5cb6f91e7ae
Fikri Faizal Hamzah Alfa'iz <fikrifaizalha@gmail.com>
2024-05-29 03:49:37 +0700

    commit

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,11 +1,41 @@
-import styles from './HeroStyle.module.css'; // pastikan path-nya sesuai
-import heroImg from '../../assets/hero.png'; // pastikan path-nya sesuai
-
+import styles from './HeroStyles.module.css';
+import heroImg from '../../assets/hero-img.png'; 
+import themeIcon from '../../assets/sun.svg';
+import TwitterIcon from '../../assets/twitter-light.svg';
+import GithubIcon from '../../assets/github-light.svg';
+import linkedinIcon from '../../assets/linkedin-light.svg';
 function Hero() {
   return (
-    <section id='hero' className={styles.hero}>
-      <div>
-        <img src={heroImg} alt="Hero" />
+    <section id='hero' className={styles.Container}>
+      <div className={styles.colorModeContainer}>
+        <img
+         className={styles.hero}
+         src={heroImg} 
+         alt="Profile picture of Fikri Faizal"
+        />
+        <img
+         className={styles.colorMode}
+         src={themeIcon}
+         alt="Color mode icon" 
+         />
+      </div>
+      <div className={styles.info}>
+        <h1>Fikri Faizal 
+          <br /> 
+          Hamzah Alfaiz
+          </h1>
+          <h2>Frontend Developer</h2>
+          <span>
+            <a href="https://twitter.com/" target='_blank'>
+              <img src={TwitterIcon} alt='Twitter icon' />
+            </a>
+            <a href="https://github.com/" target='_blank'>
+              <img src={GithubIcon} alt='Github icon' />
+            </a>
+            <a href="https://linkedin.com/" target='_blank'>
+              <img src={linkedinIcon} alt='Linkedin icon' />
+            </a>
+          </span>
       </div>
     </section>
   );
