672bedf2b44d9592337d665d1f8b27cc2aa809ba
Fikri Faizal Hamzah Alfa'iz <fikrifaizalha@gmail.com>
2024-05-29 16:44:00 +0700

    commit

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,10 +1,24 @@
 import styles from './HeroStyles.module.css';
 import heroImg from '../../assets/hero-img.png'; 
-import themeIcon from '../../assets/sun.svg';
-import TwitterIcon from '../../assets/twitter-light.svg';
-import GithubIcon from '../../assets/github-light.svg';
-import linkedinIcon from '../../assets/linkedin-light.svg';
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import twitterLight from '../../assets/twitter-light.svg';
+import twitterDark from '../../assets/twitter-dark.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import CV from "../../assets/cv.pdf";
+import { useTheme } from '../../common/ThemeContext';
+
 function Hero() {
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === 'light' ? sun : moon;
+  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+
   return (
     <section id='hero' className={styles.Container}>
       <div className={styles.colorModeContainer}>
@@ -17,6 +31,7 @@ function Hero() {
          className={styles.colorMode}
          src={themeIcon}
          alt="Color mode icon" 
+         onClick={toggleTheme}
          />
       </div>
       <div className={styles.info}>
@@ -27,15 +42,21 @@ function Hero() {
           <h2>Frontend Developer</h2>
           <span>
             <a href="https://twitter.com/" target='_blank'>
-              <img src={TwitterIcon} alt='Twitter icon' />
+              <img src={twitterIcon} alt='Twitter icon' />
             </a>
             <a href="https://github.com/" target='_blank'>
-              <img src={GithubIcon} alt='Github icon' />
+              <img src={githubIcon} alt='Github icon' />
             </a>
             <a href="https://linkedin.com/" target='_blank'>
               <img src={linkedinIcon} alt='Linkedin icon' />
             </a>
           </span>
+          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex est magni saepe dicta consequuntur!
+             Odit dolorum quaerat minima, unde quis molestias, fugiat mollitia molestiae necessitatibus hic recusandae. Quae, quia officia!
+          </p>
+          <a href={CV} download>
+            <button className='hover'>Resume</button>
+          </a>
       </div>
     </section>
   );
